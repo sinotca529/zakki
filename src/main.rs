d1219fe55110c4b67d27de3447f1cebdba904c8e
sinotca529-wsl <52117877+sinotca529@users.noreply.github.com>
2024-04-21 11:24:28 +0900

    feat: init, build, and clean command

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,110 @@
 mod convert;
 
-use convert::convert;
+use std::{
+    io::{Read, Write},
+    path::{Path, PathBuf},
+};
 
-fn main() {
-    let path = std::env::args().nth(1).unwrap();
-    println!("{}", convert(path));
+use anyhow::Result;
+use clap::{Parser, Subcommand};
+use convert::md_to_html;
+
+#[derive(Debug, Subcommand)]
+enum Command {
+    Init,
+    Build,
+    Clean,
+}
+
+#[derive(Debug, Parser)]
+struct Cli {
+    #[command(subcommand)]
+    subcommand: Command,
+}
+
+fn src_dir() -> Result<PathBuf> {
+    Ok(std::env::current_dir()?.join("src"))
+}
+
+fn build_dir() -> Result<PathBuf> {
+    Ok(std::env::current_dir()?.join("build"))
+}
+
+fn init_zakki_dir() -> Result<()> {
+    let src_dir: PathBuf = src_dir()?;
+    let demo_md_path = src_dir.join("hello.md");
+    std::fs::create_dir(src_dir)?;
+
+    std::fs::create_dir(build_dir()?)?;
+
+    std::fs::File::create(demo_md_path)?;
+    Ok(())
+}
+
+fn visit_files_recursively(
+    dir: impl AsRef<Path>,
+    operator: impl Fn(PathBuf) -> Result<()>,
+) -> Result<()> {
+    let dir = dir.as_ref();
+    let mut work_list: Vec<PathBuf> = vec![dir.into()];
+    while let Some(dir) = work_list.pop() {
+        for e in std::fs::read_dir(&dir)? {
+            let path = e?.path();
+            if path.is_dir() {
+                work_list.push(path);
+            } else {
+                operator(path)?;
+            }
+        }
+    }
+    Ok(())
+}
+
+fn html_path(md_path: impl AsRef<Path>) -> Result<PathBuf> {
+    fn inner(md_path: &Path) -> Result<PathBuf> {
+        let rel_path = md_path.strip_prefix(src_dir()?)?;
+        let html_path = build_dir()?.join(rel_path).with_extension("html");
+        Ok(html_path)
+    }
+    inner(md_path.as_ref())
+}
+
+fn read_file(path: impl AsRef<Path>) -> Result<String> {
+    let mut buf = String::new();
+    std::fs::File::open(path)?.read_to_string(&mut buf)?;
+    Ok(buf)
+}
+
+fn write_file(path: impl AsRef<Path>, content: &str) -> Result<()> {
+    let mut file = std::fs::File::create(path)?;
+    file.write_fmt(format_args!("{content}"))?;
+    Ok(())
+}
+
+fn make_html_file(md_path: PathBuf) -> Result<()> {
+    let md_content = read_file(&md_path)?;
+    let html_content = md_to_html(&md_content);
+
+    let html_path = html_path(&md_path)?;
+    write_file(html_path, &html_content)?;
+
+    Ok(())
+}
+
+fn build() -> Result<()> {
+    visit_files_recursively(src_dir()?, make_html_file)
+}
+
+fn clean_zakki_dir() -> Result<()> {
+    std::fs::remove_dir_all(build_dir()?)?;
+    Ok(())
+}
+
+fn main() -> Result<()> {
+    let cli = Cli::parse();
+    match cli.subcommand {
+        Command::Init => init_zakki_dir(),
+        Command::Build => build(),
+        Command::Clean => clean_zakki_dir(),
+    }
 }
