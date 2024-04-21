cbacc63eb7921cb0dfbc926c04b1290a514b6dd8
sinotca529-wsl <52117877+sinotca529@users.noreply.github.com>
2024-04-21 11:44:07 +0900

    fix: make dirs for generated html files

--- src/main.rs
+++ src/main.rs
@@ -76,6 +76,8 @@ fn read_file(path: impl AsRef<Path>) -> Result<String> {
 }
 
 fn write_file(path: impl AsRef<Path>, content: &str) -> Result<()> {
+    let path = path.as_ref();
+    std::fs::create_dir_all(path.parent().unwrap())?;
     let mut file = std::fs::File::create(path)?;
     file.write_fmt(format_args!("{content}"))?;
     Ok(())
