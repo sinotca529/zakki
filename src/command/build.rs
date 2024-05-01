fbdaaf7066d6187822010f04a09698f8ff55d85b
sinotca529-wsl <52117877+sinotca529@users.noreply.github.com>
2024-05-01 17:07:39 +0900

    feat: copy non-md files

--- src/command/build.rs
+++ src/command/build.rs
@@ -1,23 +1,31 @@
-use super::{html_path, relative_path_to_css, src_dir};
+use super::{dst_dir, html_path, relative_path_to_css, src_dir};
 use crate::convert::md_to_html;
 use anyhow::Result;
 use std::path::{Path, PathBuf};
 
 pub fn build() -> Result<()> {
-    visit_files_recursively(src_dir(), make_html_file)
+    visit_files_recursively(src_dir(), convert_file)
 }
 
-fn make_html_file(md_path: PathBuf) -> Result<()> {
-    let md_content = std::fs::read(&md_path)?;
-    let md_content = std::str::from_utf8(&md_content)?;
+fn convert_file(src_path: PathBuf) -> Result<()> {
+    match src_path.extension().and_then(|e| e.to_str()) {
+        Some("md") => {
+            let md_content = std::fs::read(&src_path)?;
+            let md_content = std::str::from_utf8(&md_content)?;
 
-    let html_path = html_path(&md_path)?;
+            let html_path = html_path(&src_path)?;
 
-    let path_to_css = relative_path_to_css(&html_path)?;
-    let html_content = md_to_html(md_content, path_to_css.to_str().unwrap());
+            let path_to_css = relative_path_to_css(&html_path)?;
+            let html_content = md_to_html(md_content, path_to_css.to_str().unwrap());
 
-    std::fs::write(html_path, html_content)?;
-
+            std::fs::write(html_path, html_content)?;
+        }
+        _ => {
+            let dst_path = dst_dir().join(src_path.strip_prefix(src_dir())?);
+            std::fs::create_dir_all(dst_path.parent().unwrap())?;
+            std::fs::copy(src_path, dst_path)?;
+        }
+    }
     Ok(())
 }
 
