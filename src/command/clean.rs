15c678a19454e71088039c4fd97e482fb0287036
sinotca529-wsl <52117877+sinotca529@users.noreply.github.com>
2024-05-06 18:06:31 +0900

    fix: iff the build directory exists, delete it.

--- src/command/clean.rs
+++ src/command/clean.rs
@@ -2,5 +2,8 @@ use crate::path::dst_dir;
 use anyhow::Result;
 
 pub fn clean() -> Result<()> {
+    if !dst_dir().exists() {
+        return Ok(());
+    }
     std::fs::remove_dir_all(dst_dir()).map_err(Into::into)
 }
