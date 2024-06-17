80464195d73d23b888118d2963542724f46b99be
Takumi Watanabe <52117877+sinotca529@users.noreply.github.com>
2024-06-17 18:17:53 +0900

    misc: remove unused comments

--- src/command/init.rs
+++ src/command/init.rs
@@ -2,14 +2,6 @@ use crate::{copy_asset, util::write_file};
 use anyhow::Result;
 
 pub fn init() -> Result<()> {
-    // let pwd = std::env::current_dir()?;
-    // write_file(pwd.join("config.toml"), read_asset!("config.toml"))?;
-    // Ok(())
-
-    // copy_asset!("style.css")?;
-    // copy_asset!("script.js")?;
-    // copy_asset!("index.html")?;
-    // copy_asset!("tag.html")?;
     copy_asset!("config.toml", "")?;
     Ok(())
 }
