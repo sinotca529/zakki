79187453783eca2bc6e571c8c17a9ce595c3be6f
Takumi Watanabe <52117877+sinotca529@users.noreply.github.com>
2024-08-02 23:48:25 +0900

    fix: try to remove build dir iff the dir is existing

--- src/command/clean.rs
+++ src/command/clean.rs
@@ -1,8 +1,13 @@
 use super::ensure_pwd_is_book_root_dir;
 use crate::path::dst_dir;
 use anyhow::Result;
+use std::fs::remove_dir_all;
 
 pub fn clean() -> Result<()> {
     ensure_pwd_is_book_root_dir()?;
-    std::fs::remove_dir_all(dst_dir()).map_err(Into::into)
+    if dst_dir().exists() {
+        remove_dir_all(dst_dir()).map_err(Into::into)
+    } else {
+        Ok(())
+    }
 }
