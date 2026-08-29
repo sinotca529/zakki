813606eeed2a7d5c928f2d7bd6f5ecc292e05a02
Takumi Watanabe <52117877+sinotca529@users.noreply.github.com>
2026-08-29 22:09:03 +0900

    refactor: figure タグの作成処理を切り出し

--- src/command/build/renderer/pass/convert_image.rs
+++ src/command/build/renderer/pass/convert_image.rs
@@ -3,7 +3,7 @@ use comrak::nodes::{AstNode, NodeValue};
 
 /// 画像を `<figure>` で囲み、alt テキストを `<figcaption>` にします。
 pub fn convert_image<'a>(root: &'a AstNode<'a>) -> anyhow::Result<()> {
-    let targets: Vec<_> = root
+    let img_nodes: Vec<_> = root
         .descendants()
         .filter_map(|node| match &node.data().value {
             NodeValue::Image(link) => {
@@ -13,41 +13,42 @@ pub fn convert_image<'a>(root: &'a AstNode<'a>) -> anyhow::Result<()> {
         })
         .collect();
 
-    for (node, url, title, alt) in targets {
-        let alt = (!alt.is_empty()).then_some(alt);
-
-        let img_tag = make_image_tag(&url, &alt, &title);
-        let figcaption_tag = alt
-            .as_ref()
-            .map(|alt| format!(r#"<figcaption>{}</figcaption>"#, escape_html_attr(alt)))
-            .unwrap_or_default();
-
-        // 子ノード (alt テキスト) は figure に取り込んだので取り除く
+    for (node, url, title, alt) in img_nodes {
+        // 子ノード (alt テキスト) は figure に取り込むので取り除く
         node.children().for_each(|child| child.detach());
-        node.data_mut().value = NodeValue::HtmlInline(format!(
-            r#"<figure><div class="zakki-scroll">{img_tag}</div>{figcaption_tag}</figure>"#
-        ));
+
+        // figure タグの追加
+        let figure_tag = make_figure_tag(&url, &alt, &title);
+        node.data_mut().value = NodeValue::HtmlInline(figure_tag);
     }
 
     Ok(())
 }
 
-fn make_image_tag(url: &str, alt: &Option<String>, title: &str) -> String {
-    // 文字列を選択できるようにするため、 SVG は object ノードで囲む
-    if url.ends_with(".svg") {
-        return format!(r#"<object type="image/svg+xml" data="{url}"></object>"#);
-    }
+fn make_figure_tag(url: &str, alt: &str, title: &str) -> String {
+    let alt = (!alt.is_empty()).then_some(alt);
+    let title = (!title.is_empty()).then_some(title);
 
-    let alt_attr = alt
-        .as_ref()
-        .map(|t| format!(r#" alt="{}""#, escape_html_attr(t)))
-        .unwrap_or_default();
-
-    let title_attr = if title.is_empty() {
-        String::new()
+    let img_tag = if url.ends_with(".svg") {
+        // 文字列を選択できるようにするため、 SVG は object ノードで囲む
+        format!(r#"<object type="image/svg+xml" data="{url}"></object>"#)
     } else {
-        format!(r#" title="{}""#, escape_html_attr(title))
+        let alt_attr = alt
+            .as_ref()
+            .map(|t| format!(r#" alt="{}""#, escape_html_attr(t)))
+            .unwrap_or_default();
+
+        let title_attr = title
+            .map(|title| format!(r#" title="{}""#, escape_html_attr(title)))
+            .unwrap_or_default();
+
+        format!(r#"<img loading="lazy" src="{url}"{alt_attr}{title_attr}/>"#)
     };
 
-    format!(r#"<img loading="lazy" src="{url}"{alt_attr}{title_attr}/>"#)
+    let figcaption_tag = alt
+        .as_ref()
+        .map(|alt| format!(r#"<figcaption>{}</figcaption>"#, escape_html_attr(alt)))
+        .unwrap_or_default();
+
+    format!(r#"<figure><div class="zakki-scroll">{img_tag}</div>{figcaption_tag}</figure>"#)
 }
