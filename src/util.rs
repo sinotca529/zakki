59e8a57d3687a2b9e8be7b33a6a1bcf2b1a539b8
Takumi Watanabe <52117877+sinotca529@users.noreply.github.com>
2026-09-06 23:49:22 +0900

    fix: copy_asset マクロが呼び出し元の処理そのものを return させてしまう問題を修正

--- src/util.rs
+++ src/util.rs
@@ -26,13 +26,13 @@ macro_rules! copy_asset {
     ($fname:expr, $to:expr) => {{
         let path = $to.join($fname);
         if path.exists() {
-            return Ok(());
+            Ok(())
+        } else {
+            $crate::util::write_file(
+                path,
+                include_bytes!(concat!(env!("CARGO_MANIFEST_DIR"), "/asset/", $fname)),
+            )
+            .with_context(|| anyhow!("Failed to copy {}", $fname))
         }
-
-        $crate::util::write_file(
-            path,
-            include_bytes!(concat!(env!("CARGO_MANIFEST_DIR"), "/asset/", $fname)),
-        )
-        .with_context(|| anyhow!("Failed to copy {}", $fname))
     }};
 }
